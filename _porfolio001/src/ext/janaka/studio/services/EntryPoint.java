b74abc8ea15fc3e8a02e2afdeec1c5ded9f93892
Janaka-Ar-Studio <janakaarstudio@gmail.com>
2024-05-20 19:48:44 +0530

    Second Commit

--- _porfolio001/src/ext/janaka/studio/services/EntryPoint.java
+++ _porfolio001/src/ext/janaka/studio/services/EntryPoint.java
@@ -5,7 +5,7 @@ public class EntryPoint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Welcome to JANAKA STUDIO!!");
+		System.out.println("Welcome to JANAKA STUDIO Again Welcome!!");
 	}
 
 }
