82cbc099dbf097e802b77e10c1b318f6b0c56b78
ajayraman <ajayraman@localhost>
2024-05-20 19:10:45 +0530

    First/Initial "PLM Studio Commit"

---
+++ _porfolio001/src/ext/janaka/studio/services/EntryPoint.java
@@ -0,0 +1,11 @@
+package ext.janaka.studio.services;
+
+public class EntryPoint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Welcome to JANAKA STUDIO!!");
+	}
+
+}
